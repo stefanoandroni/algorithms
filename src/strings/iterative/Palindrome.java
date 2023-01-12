429534a46253a16ac98f960bc5d5e42fdc38bfa1
Stefano Androni <65281018+stefanoandroni@users.noreply.github.com>
2023-01-12 17:32:32 +0100

    Add: strings.iterative.Palindrome

---
+++ src/strings/iterative/Palindrome.java
@@ -0,0 +1,24 @@
+package strings.iterative;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Palindrome.isPalindrome("SAGAS"));
+        System.out.println(Palindrome.isPalindrome("ABBCA"));
+    }
+
+}
